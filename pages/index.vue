796ac81da1280dfb3d8ba1f289fc03b83e6f2c39
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2024-11-19 16:06:00 +0100

    video intro + overlay

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,35 @@
 <template>
     <main>
-        <div class="p-5">
-            <img class="w-full" src="../assets/images/bitmap/home-test.png" alt="">
+        <div class="my-[2vh]">
+            <div class="absolute w-[97vw] left-1/2 -translate-x-1/2 bg-black opacity-40 rounded-xl" :style="{ height: containerHeight + 'px' }"></div>
+            <video id="intro" autoplay loop muted src="../assets/videos/Video_introduction_MMI/Video_introduction_MMI.mp4" class="rounded-xl w-[97vw] mx-auto"></video>
+        </div>
+        <div class="static ">
+            <p class="">bla</p>
         </div>
     </main>
 </template>
+
+
+<script setup lang="ts">
+const containerHeight = ref(0);
+
+const calculateHeight = () => {
+    const width = window.innerWidth * 0.97;
+    let height = Math.floor((width * 9) / 16);
+    // if (width >= 1020) {
+    //     height -= 10;
+    // }
+    containerHeight.value = height;
+};
+
+onMounted(() => {
+  calculateHeight();
+  window.addEventListener("resize", calculateHeight); 
+});
+
+onUnmounted(() => {
+  window.removeEventListener("resize", calculateHeight); 
+});
+
+</script>
