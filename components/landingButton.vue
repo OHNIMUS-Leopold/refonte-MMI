27b103f99876d771d80709593488caa74a3283c8
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2024-11-20 11:08:28 +0100

    landing btn

---
+++ components/landingButton.vue
@@ -0,0 +1,11 @@
+<template>
+    <RouterLink :to="to" class="duration-500 font-poppins text-xl font-bold py-3 px-10 text-white bg-transparent border-solid border-[3px] border-jaune hover:bg-jaune rounded-[10px]">
+        <slot />
+    </RouterLink>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    to: string;
+}>();
+</script>
