d3389110d8a5a1d367ec33de13ae6a45a191d487
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2024-12-06 17:14:12 +0100

    video home styling

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,11 @@
             <div class="absolute w-[97vw] left-1/2 -translate-x-1/2 bg-black opacity-40 rounded-xl" :style="{ height: containerHeight + 'px' }"/>
             <video id="intro" autoplay loop muted src="@/assets/videos/Video_introduction_MMI/Video_introduction_MMI.mp4" class="rounded-xl w-[97vw] mx-auto"/>
         </div>
-        <div class="static ">
+        <div class="absolute w-full" :style="{ top: containerHeight / 4 + 'px'}">
+            <img class="h-24 mx-auto mb-16" src="@/assets/images/svg/mmi_logo-white.svg" alt="Logo">
+            <h1 class="h1 uppercase text-white text-center">MMI Montbéliard</h1>
+        </div>
+        <div class="static">
             <p class="">bla</p>
         </div>
     </main>
