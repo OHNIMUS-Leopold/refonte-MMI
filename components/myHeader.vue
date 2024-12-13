579c31f2e77270749211f4718f720d94f3d9ee73
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2024-12-13 10:43:34 +0100

    responsive mobile header

--- components/myHeader.vue
+++ components/myHeader.vue
@@ -1,25 +1,27 @@
 <script setup lang="ts">
-const headerBreakpoint = ref(false);
+const isMenuOpen = ref(false);
+const menuTop = ref(0);
 const isAnimated = ref(false);
 const isHidden = ref(false);
 const lastScrollTop = ref(0);
 
-const updateScreenWidth = () => {
-    if (typeof window !== 'undefined') {
-        headerBreakpoint.value = window.innerWidth > 1280;
+const toggleMenu = () => {
+    isMenuOpen.value = !isMenuOpen.value;
+    if (isMenuOpen.value) {
+        menuTop.value = window.scrollY; // Définit la position actuelle du scroll
+        document.body.classList.add('overflow-hidden');
+    } else {
+        document.body.classList.remove('overflow-hidden');
     }
 };
 
 onMounted(() => {
-    updateScreenWidth(); 
     isAnimated.value = true;
     window.addEventListener('scroll', handleScroll);
-    window.addEventListener('resize', updateScreenWidth);
 });
 
 onUnmounted(() => {
     window.removeEventListener('scroll', handleScroll);
-    window.removeEventListener('resize', updateScreenWidth);
 });
 
 const handleScroll = () => {
@@ -36,7 +38,7 @@ const handleScroll = () => {
 </script>
 
 <template>
-    <header v-if="headerBreakpoint" class="fixed top-8 left-1/2 -translate-x-1/2 z-10 bg-white py-2 px-4 rounded-[44px] w-4/5 mx-auto drop-shadow-lg" :class="{ 'expand': isAnimated, 'hide-expand': isHidden }">
+    <header class="hidden xl:block fixed top-8 left-1/2 -translate-x-1/2 z-10 bg-white py-2 px-4 rounded-[44px] w-4/5 mx-auto drop-shadow-lg" :class="{ 'expand': isAnimated, 'hide-expand': isHidden }">
         <nav class="text-gris text-xl font-poppins font-semibold flex justify-between items-center" :class="{ 'drop': isAnimated, 'hide-drop': isHidden }">
             <NuxtLink class="relative" to="/">
                 <img src="@/assets/images/svg/mmi_logo-header.svg" alt="Logo" class="h-8 pl-2 pb-1">
@@ -49,14 +51,51 @@ const handleScroll = () => {
         </nav>
     </header>
 
-    <header v-else class="absolute">
-        ddz
+    <header class="xl:hidden absolute top-0 left-0 z-10 w-full">
+        <div class="flex items-center justify-between p-6">
+            <img class="h-10" src="@/assets/images/svg/mmi_logo-white.svg" alt="Logo">
+            <button @click="toggleMenu">
+                <img class="h-10" src="@/assets/images/svg/icn/open-menu.svg" alt="Ouvrir le menu">
+            </button>
+        </div>
+
+
+        <nav v-if="isMenuOpen" class="absolute top-0 left-0 z-20 bg-white h-screen w-full p-6 pl-11 text-gris text-xl font-poppins font-semibold grid grid-cols-1 justify-items-start gap-12 content-start" :style="{ top: menuTop + 'px' }">
+            <div class="flex justify-between w-full items-start">
+                <p class="font-poppins font-bold text-4xl text-noir mt-16">MENU</p>
+                <button @click="toggleMenu">
+                    <img src="@/assets/images/svg/icn/close-menu.svg" alt="Fermer le menu">
+                </button>
+            </div>
+            <NuxtLink @click="toggleMenu" class="relative animate" to="/">Accueil</NuxtLink>
+            <NuxtLink @click="toggleMenu" class="relative animate" to="/la-formation">La Formation</NuxtLink>
+            <NuxtLink @click="toggleMenu" class="relative animate" to="/le-departement">Le Département</NuxtLink>
+            <NuxtLink @click="toggleMenu" class="relative animate" to="/international">International</NuxtLink>
+            <NuxtLink @click="toggleMenu" class="relative animate" to="/contact">Contact</NuxtLink>
+            <hr class="border-t-2 border-noir" style="width: calc(100% - 20px);" />
+            <NuxtLink @click="toggleMenu" to="/candidater" class="font-medium text-lg font-manrope py-1 px-6 border-solid border-[3px] border-jaune bg-jaune rounded-3xl hover:bg-white duration-500">Candidater</NuxtLink>    
+            <div class="absolute bottom-5 left-1/2 -translate-x-1/2 flex justify-between space-x-6 mx-auto">
+                <a href="https://www.facebook.com/mmimontbeliard/?locale=fr_FR">
+                    <img class="h-8" src="@/assets/images/svg/icn/icon-facebook.svg" alt="Facebook">
+                </a>
+                <a href="https://www.linkedin.com/school/mmimontbeliard">
+                    <img class="h-8" src="@/assets/images/svg/icn/icon-linkedin.svg" alt="LinkedIn">
+                </a>
+                <a href="https://www.instagram.com/mmi_montbeliard/">
+                    <img class="h-8" src="@/assets/images/svg/icn/icon-instagram.svg" alt="Instagram">
+                </a>
+            </div>
+        </nav>
     </header>
 </template>
 
 
 
 <style>
+
+.overflow-hidden {
+    overflow: hidden;
+}
 
 .animate::after {
     content: '';
@@ -158,6 +197,9 @@ const handleScroll = () => {
     }
     10% { 
         opacity: 0;
+    }
+    45% {
+        font-size: 0px;
     }
     50% {
         transform: translateY(-200px); 
