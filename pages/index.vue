2c170014e7e70c78ebdf05d5cabfb7951947b5d5
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2025-01-10 14:58:28 +0100

    comment

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,16 @@
 <template>
     <main>
         <div class="my-[1%]">
-            <div class="absolute w-full max-w-[98%] mx-[1%] bg-black opacity-40 rounded-xl" :style="{ height: containerHeight + 'px' }"/>
+            <div class="absolute w-full max-w-[98%] mx-[1%] bg-black opacity-[45%] rounded-xl" :style="{ height: containerHeight + 'px' }"/>
             <video id="intro" autoplay loop muted src="@/assets/videos/Video_introduction_MMI/Video_introduction_MMI.mp4" class="rounded-xl max-w-[98%] mx-[1%]"/>
         </div>
-        <!-- <div class="absolute w-full" :style="{ top: containerHeight / 4 + 'px'}">
-            <img class="h-24 mx-auto mb-16" src="@/assets/images/svg/mmi_logo-white.svg" alt="Logo">
-            <h1 class="h1 uppercase text-white text-center">MMI Montbéliard</h1>
+        <!-- <div class="hidden xl:block absolute w-full" :style="{ top: containerHeight / 4 + 'px'}">
+            <img class="h-24 mx-auto" src="@/assets/images/svg/mmi_logo-white.svg" alt="Logo">
+            <h1 class="uppercase text-white text-center mt-16 mb-14">MMI Montbéliard</h1>
+            <div class="flex justify-center space-x-24 mt-8">
+                <LandingButton to="/la-formation">Découvrir la formation</LandingButton>
+                <LandingButton to="/candidater">Candidater</LandingButton>
+            </div>
         </div> -->
         <div class="static">
             <p class="">bla</p>
