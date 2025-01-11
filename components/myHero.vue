198bcd1fa100a46c2926a75591a997f9e11d8650
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2025-01-11 13:20:48 +0100

    hero tailles

--- components/myHero.vue
+++ components/myHero.vue
@@ -19,15 +19,15 @@ const getLink = (path: string) => {
             <img :src=image class="object-cover max-h-full w-full rounded-xl" :alt=alt>
         </div>
         <img class="hidden xl:block absolute top-[24%] left-1/2 -translate-x-1/2 h-24" src="@/assets/images/svg/mmi_logo-white.svg" alt="Logo MMI">
-        <h1 class="uppercase text-white absolute top-[22%] xl:top-[48%] left-1/2 -translate-x-1/2">
+        <h1 class="uppercase text-white absolute top-[22%] md:top-[17%] xl:top-[48%] left-1/2 -translate-x-1/2">
             {{title}}
         </h1>
         <div class="absolute top-[34%] xl:top-[70%] w-full text-center">
-            <div class="block xl:hidden space-x-[16%]">
+            <div class="block md:hidden space-x-[16%]">
                 <LandingButtonMini to="/la-formation">Découvrir la formation</LandingButtonMini>
                 <LandingButtonMini :to="getLink('/candidater')">Candidater</LandingButtonMini>
             </div>
-            <div class="hidden xl:block space-x-[16%]">
+            <div class="hidden md:block space-x-[16%]">
                 <LandingButton to="/la-formation">Découvrir la formation</LandingButton>
                 <LandingButton :to="getLink('/candidater')">Candidater</LandingButton>
             </div>
