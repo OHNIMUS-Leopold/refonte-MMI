57c0c70405c723e6ba3548b19ee5ca48c56977c1
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2025-01-13 13:01:50 +0100

    discover button

---
+++ components/discoverButton.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+    to: string;
+}>();
+</script>
+
+
+<template>
+    <NuxtLink :to="to" class="contents">
+        <div class="flex items-center space-x-16 text-noir py-2 px-7 border-solid border-[1px] border-noir w-fit">
+            <p>Découvrir </p>
+            <p>→</p>
+        </div>
+    </NuxtLink>
+</template>
