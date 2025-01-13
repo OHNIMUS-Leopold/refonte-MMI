9ed99630692d16d7f2916a93ba2af243b8655139
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2025-01-13 20:26:05 +0100

    lint debug

--- components/simpleSection.vue
+++ components/simpleSection.vue
@@ -12,9 +12,7 @@ defineProps<{
     <section class="grid grid-cols-1 xl:grid-cols-5 xl:gap-14 mb-12 md:mb-24">
         <div class="col-span-3 xl:flex xl:flex-col xl:gap-4">
             <AnimatedHeading class="text-noir mb-8" :title="headingTitle" />
-            <p class="text-noir my-5 xl:m-0" v-html="paragraphText">
-
-            </p>
+            <p class="text-noir my-5 xl:m-0" v-html="paragraphText"/>
         </div>
         <img class="col-span-2 xl:place-self-end w-auto mx-auto" :src="imageSrc" :alt="imageAlt">
     </section>
