c5df58b273f69d37ecc20f07fad1176fe29d2fd5
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2025-01-14 11:20:44 +0100

    transparent button yellow component

---
+++ components/transparentButtonY.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+    to: string;
+}>();
+</script>
+
+
+<template>
+    <RouterLink :to="to" class="duration-500 font-poppins text-xl font-medium p-3 text-jaune bg-transparent border-solid border-[4px] border-jaune hover:bg-jaune hover:text-white rounded-[10px]">
+        <slot />
+    </RouterLink>
+</template>
