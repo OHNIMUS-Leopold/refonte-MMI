a71fce0e5dd2768a0b8507e52cef72bf088d4854
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2025-01-14 10:49:34 +0100

    présentation des années composant

---
+++ components/yearsTable.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="hidden lg:grid mt-20 grid-cols-3 justify-items-center items-start">
+        <div class="text-center w-64">
+            <p class="font-poppins text-5xl text-noir font-bold mb-28">1ère <br>Année</p>
+            <div class="grid grid-cols-1 gap-24">
+                <p class="font-manrope text-2xl text-noir font-extrabold p-5">Création numérique</p>
+                <p class="font-manrope text-2xl text-noir font-extrabold">Stratégie <br>de communication</p>
+                <p class="font-manrope text-2xl text-noir font-extrabold">Développement web</p>
+            </div>
+        </div>
+        <div class="text-center w-64">
+            <p class="font-poppins text-5xl text-noir font-bold mb-28">2ème <br>Année</p>
+            <div class="grid grid-cols-1 gap-24">
+                <p class="font-manrope text-2xl text-noir font-extrabold border-4 border-jaune rounded p-5">Création numérique</p>
+                <p class="font-manrope text-2xl text-noir font-extrabold">Stratégie <br>de communication</p>
+                <p class="font-manrope text-2xl text-noir font-extrabold">Développement web</p>
+            </div>
+        </div>
+        <div class="text-center w-64">
+            <p class="font-poppins text-5xl text-noir font-bold mb-28">3ème <br>Année</p>
+            <div class="grid grid-cols-1 gap-24 border-4 border-jaune rounded pt-20 pb-10">
+                <p class="font-manrope text-2xl text-noir font-extrabold"><br></p>
+                <p class="font-manrope text-2xl text-noir font-extrabold">Création <br>Numérique</p>
+                <p class="font-manrope text-2xl text-noir font-extrabold"><br></p>
+            </div>
+        </div>
+    </div>
+</template>
