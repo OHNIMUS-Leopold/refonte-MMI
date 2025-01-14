816a9d7e515203a79fdcacc1238bc135abaf5487
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2025-01-14 23:15:41 +0100

    récupération des actualités

--- pages/le-departement.vue
+++ pages/le-departement.vue
@@ -1,3 +1,32 @@
+<script setup lang="ts">
+import type { SanityDocument } from '@sanity/client';
+
+const ACTUALITES_QUERY = groq`*[_type == "actualite"] | order(publishedAt desc)[0...2] {
+    _id,
+    title,
+    "imageUrl": image.asset->url,
+    description,
+    publishedAt
+}`;
+
+const { data: actualite } = await useSanityQuery<SanityDocument[]>(ACTUALITES_QUERY);
+
+
+
+</script>
+
+
+
 <template>
-    <div/>
+    <main>
+        <div class="py-20 grid grid-cols-2">
+            <div v-for="actu in actualite" :key="actu._id" class="">
+                <div class="">
+                    <img :src="actu.imageUrl" :alt="actu.title" class="">
+                    <p class="">{{ actu.title }}</p>
+                    <p class="">{{ actu.description }}</p>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
