b39c350dc4b7586bd2973d02f56639c45aa29e9f
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2025-01-14 23:21:51 +0100

    utilisation du composable

--- pages/le-departement.vue
+++ pages/le-departement.vue
@@ -4,14 +4,14 @@ import type { SanityDocument } from '@sanity/client';
 const ACTUALITES_QUERY = groq`*[_type == "actualite"] | order(publishedAt desc)[0...2] {
     _id,
     title,
-    "imageUrl": image.asset->url,
+    image,
     description,
     publishedAt
 }`;
 
 const { data: actualite } = await useSanityQuery<SanityDocument[]>(ACTUALITES_QUERY);
 
-
+const {urlFor} = useSanityImage()
 
 </script>
 
@@ -22,7 +22,7 @@ const { data: actualite } = await useSanityQuery<SanityDocument[]>(ACTUALITES_QU
         <div class="py-20 grid grid-cols-2">
             <div v-for="actu in actualite" :key="actu._id" class="">
                 <div class="">
-                    <img :src="actu.imageUrl" :alt="actu.title" class="">
+                    <img v-if="actu.image" :src="urlFor(actu.image)!.url()" :alt="actu.title" class="">
                     <p class="">{{ actu.title }}</p>
                     <p class="">{{ actu.description }}</p>
                 </div>
