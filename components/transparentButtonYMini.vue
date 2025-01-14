f0a6110d64a47f89dc06419b135b79bcc33aeda6
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2025-01-14 11:20:58 +0100

    transparent button mini v. yellow component

---
+++ components/transparentButtonYMini.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+    to: string;
+}>();
+</script>
+
+
+<template>
+    <RouterLink :to="to" class="duration-500 font-poppins text-sm font-medium py-2 px-3 text-jaune bg-transparent border-solid border-2 border-jaune hover:bg-jaune hover:text-white rounded-[10px]">
+        <slot />
+    </RouterLink>
+</template>
